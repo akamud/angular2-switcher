1eb0efcc4d0e453ce1cd2f918d214fbfc763b4ce
infinity1207 <infinity1207@163.com>
2016-10-09 10:52:22 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # angular2-switcher
+
 Switch .ts/.html in angular2 project.
+
+## Usage
+
+Press 'Alt + o' will open corresponding file when you are editing in .ts/.html.
+
+## Release Notes
+
+### 0.0.2
+
+Modify categories.
+
+### 0.0.1
+
+Initial release.
---
+++ src/extension.ts
@@ -0,0 +1,54 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "angular2-switcher" is now active!');
+
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.switch', () => {
+        // The code you place here will be executed every time your command is executed
+
+        // Display a message box to the user
+        // vscode.window.showInformationMessage('Hello World!');
+
+        if (!vscode.workspace) {
+            return;
+        }
+        
+        var editor = vscode.window.activeTextEditor;
+        if (!editor) {
+            return;
+        }
+
+        var currentFile = editor.document.fileName;
+        var targetFile = '';
+        if (currentFile.toLowerCase().endsWith('.ts')) {
+            targetFile = currentFile.slice(0, currentFile.lastIndexOf('.')) + '.html';
+        }
+        else if (currentFile.toLowerCase().endsWith('.html')) {
+            targetFile = currentFile.slice(0, currentFile.lastIndexOf('.')) + '.ts';
+        }
+        else {
+            return;
+        }
+
+        vscode.workspace.openTextDocument(targetFile).then((value) => {
+            vscode.window.showTextDocument(value);
+        });
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
